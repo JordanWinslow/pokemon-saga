cc6bad1f91652a36e39c91b5a43b1ce0e744e96f
Jordan Winslow <admin@jordanwinslow.me>
2019-11-23 14:01:17 -0800

    added try catch and error type

--- src/redux/sagas/rootSaga.js
+++ src/redux/sagas/rootSaga.js
@@ -1,15 +1,18 @@
 import { call, take, put, apply } from "redux-saga/effects"
 import API from "../../API" // TODO
-import types from "../types"
+import {REQUEST_POKEMON, POKEMON_RETRIEVED, RETRIEVAL_ERROR} from "../types"
 
 // WATCHER SAGA
 export default function* getPokemonSaga() {
-  yield take(types.REQUEST_POKEMON, requestPokemonSaga)
+  yield take(REQUEST_POKEMON, requestPokemonSaga)
 }
 // WORKER SAGA
 function* requestPokemonSaga() {
+  try { // using call and apply isn't technically necessary but supposedly it makes testing easier?
   const response = yield call(fetch, API.requestPokemon) // TODO - IMPLEMENT API CLASS WITH requestPokemon ENDPOINT
   const pokemon = yield apply(response, response.json)
-  yield put({ type: types.POKEMON_RETRIEVED, payload: pokemon })
+  yield put({ type: POKEMON_RETRIEVED, payload: pokemon })
+  } catch (e) {
+    yield put({ type: RETRIEVAL_ERROR, payload: e })
+  }
 }
-
